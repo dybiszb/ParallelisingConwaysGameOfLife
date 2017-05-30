cad844cd3adc4d5b1079b6f4876955a58dbc0b0e
dybiszb <bartek.dybisz@gmail.com>
2017-05-30 20:21:46 +0200

    Docstring for dumping procedure.

--- code/conway_lib/io.h
+++ code/conway_lib/io.h
@@ -29,7 +29,26 @@ namespace cgol {
      */
     cgol::GameGridSharedPtrT loadGridFromFile(const std::string &path);
 
-
+    /**
+     * Saves consecutive grid states of the game to specified file.
+     * Output file structure is as follows:
+     * ------- file.txt --------
+     * 3
+     * 2
+     *
+     * 1 0 0
+     * 0 1 0
+     *
+     * 0 0 0
+     * 0 0 0
+     * -------------------------
+     * First two rows must be filled with grid's width and height.
+     * Next, each state of a grid is dumped and separated by whitespace.
+     * Grid is written row by row and its values are also white-space separated.
+     *
+     * @param steps Consecutive steps/grid states of a game.
+     * @param path  Path to output file.
+     */
     void dumpGameHistory(const StepsHistoryT &steps,
                          const std::string &path);
 
